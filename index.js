17e5c7169609b25c4b2e7c5d53b8baeb5c1954bf
Aditya <adityakale3@gmail.com>
2020-05-14 18:40:45 +0530

    EJS POST GET all files

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 app.use(express.static('views'));
+app.set('view engine', 'ejs');
+/*
+app.use('/assets', express.static('views'));
+*/
+var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 app.get('/', (req , res) => {
     res.send('Working');
@@ -10,8 +16,17 @@ app.get('/home', (req , res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
 
-app.get('/user/:id', (req , res) => {
-    res.send('You have entered following ID : ' + req.params.id);
+app.get('/user/:name', (req , res) => {
+    res.render('profile', {person: req.params.name});
+});
+
+app.get('/contact', (req , res) => {
+    res.render('contact', {qs: req.query});
+});
+
+app.post('/contact', urlencodedParser, (req , res) => {
+    console.log(req.body);
+    res.render('contact', {qs: req.query});
 });
 
 
